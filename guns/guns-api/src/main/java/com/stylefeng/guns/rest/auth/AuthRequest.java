c62a46788bc091b4a03f9b71698564331798ba5f
zhoubing <1050015785@qq.com>
2019-10-15 20:26:28 +0800

    周兵 更改过期时间

---
+++ guns/guns-api/src/main/java/com/stylefeng/guns/rest/auth/AuthRequest.java
@@ -0,0 +1,41 @@
+package com.stylefeng.guns.rest.auth;
+
+import java.io.Serializable;
+
+/**
+ * 认证的请求dto
+ *
+ * @author fengshuonan
+ * @Date 2017/8/24 14:00
+ */
+public class AuthRequest implements Credence, Serializable {
+
+    private String userName;
+    private String password;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public String getCredenceName() {
+        return this.userName;
+    }
+
+    @Override
+    public String getCredenceCode() {
+        return this.password;
+    }
+}
