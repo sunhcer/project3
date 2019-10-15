e614eb3c5aef009dd158a2e7eb974b9e51a62737
zhoubing <1050015785@qq.com>
2019-10-15 17:48:20 +0800

    周兵 提交搜索

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.rest.film.model.BaseFilmResponseVO;
 import com.stylefeng.guns.rest.film.service.FilmService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,12 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class FilmController {
-    @Reference(interfaceClass = FilmService.class)
+    @Reference(interfaceClass = FilmService.class, check = false)
     FilmService filmService;
     ///film/getIndex
     @GetMapping("/film/getIndex")
     public BaseFilmResponseVO getFilmIndex(){
         BaseFilmResponseVO filmIndex = filmService.getFilmIndex();
         return filmIndex;
+    }
+
+    ///film/films/{影片编号或影片名称}
+    @GetMapping(value = "/film/films/{info}")
+    public BaseFilmResponseVO<Object> getFilmInfo(Integer searchType, @PathVariable("info")String info){
+        BaseFilmResponseVO<Object> responseVO = new BaseFilmResponseVO<>();
+        if (searchType == 0){
+            //按照编号找
+            responseVO = filmService.searchFilmById(info);
+        }else{
+            //按照名称找
+            responseVO = filmService.searchFilmById(info);
+        }
+        return responseVO;
     }
 }
